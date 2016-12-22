d1c614454692ad12b6bff9dafb7151cfcfea52a9
tracer-ics2016 <tracer@njuics.org>
2016-12-22 08:22:36 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  08:22:36 up 7 days,  4:35,  1 user,  load average: 3.09, 3.15, 3.14 ff2609f5a9f6fea370c23aea4b27a1b1365789f3

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -230,7 +230,8 @@ helper_fun _2byte_opcode_table [256] = {
 make_helper(exec) {
 	ops_decoded.opcode = instr_fetch(eip, 1);//fetch the instruction from memory
 	Log("opcode = %d\n", ops_decoded.opcode);
-	Log("%s\n", str_temp(make_helper_v(mov_i2r)));
+#define PRINT_MACRO(x) #x
+#pragma message(PRINT_MACRO(s))
 	return opcode_table[ ops_decoded.opcode ](eip);
 }
 
