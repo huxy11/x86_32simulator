1aafb31c18e079af5ba1a7c305e4e90640a3a4cd
tracer-ics2016 <tracer@njuics.org>
2016-12-24 09:33:47 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  09:33:47 up 9 days,  5:46,  2 users,  load average: 3.19, 3.16, 3.15 b3de6ee88082bd8f0cae710615421493fdcde68

--- nemu/src/cpu/exec/stack/push-template.h
+++ nemu/src/cpu/exec/stack/push-template.h
@@ -3,11 +3,9 @@
 #define instr push
 
 static void do_execute() {
-	//TODO
-	Log("type = %d\n\tsize = %d\n\tunion = %x\n\tval = %x\n\tstr = %s\n\t", op_src->type, op_src->size, op_src->imm, op_src->val, op_src->str);
-	Log("before ebp = %x\n", REG(4));
+	//Log("type = %d\n\tsize = %d\n\tunion = %x\n\tval = %x\n\tstr = %s\n\t", op_src->type, op_src->size, op_src->imm, op_src->val, op_src->str);
 	REG(4) += DATA_BYTE;
-	Log("after ebp = %x\n", REG(4));
+	swaddr_write(REG(4), DATA_BYTE, op_src->val);
 	print_asm_template2();
 }
 
