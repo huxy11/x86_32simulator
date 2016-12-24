775d37030d01acacca91a370e286f2d8a177f82d
tracer-ics2016 <tracer@njuics.org>
2016-12-24 09:22:52 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  09:22:52 up 9 days,  5:35,  2 users,  load average: 3.12, 3.12, 3.13 ce5e3134475cb7e40c662553d30522ae36c2db7

--- nemu/src/cpu/exec/stack/push-template.h
+++ nemu/src/cpu/exec/stack/push-template.h
@@ -5,6 +5,7 @@
 static void do_execute() {
 	//TODO
 	Log("type = %d\n\tsize = %d\n\tunion = %x\n\tval = %x\n\tstr = %s\n\t", op_src->type, op_src->size, op_src->imm, op_src->val, op_src->str);
+	Log("ebp = %x\n", REG(5));
 	print_asm_template2();
 }
 
