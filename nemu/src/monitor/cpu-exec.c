a1ad75408b7ed8723c58a97bf5099cbad7a04c77
tracer-ics2016 <tracer@njuics.org>
2016-12-24 06:55:02 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  06:55:02 up 9 days,  3:08,  1 user,  load average: 3.00, 3.07, 3.12 d4938d1156f3fc3b74f196e928c97d6fb8e36b2b

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -63,7 +63,7 @@ void cpu_exec(volatile uint32_t n) {
 		int instr_len = exec(cpu.eip);
 
 		cpu.eip += instr_len;
-		Log("Now eip = %x\n", cpu.eip);
+		Log("Now eip = 0x%x\n", cpu.eip);
 
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
