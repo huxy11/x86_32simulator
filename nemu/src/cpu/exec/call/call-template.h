f4e8e1b07ea8c443de08d879e8a540b592e3000b
tracer-ics2016 <tracer@njuics.org>
2016-12-26 02:19:18 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  02:19:18 up  1:43,  2 users,  load average: 0.13, 0.08, 0.05 9cc4405fdcbb965cf459aa5c96c49fb328d41418

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -1,10 +1,10 @@
 #include "cpu/exec/template-start.h"
-
+#include "cpu/reg.h"
 #define instr call
 
 static void do_execute() {
 	Log("op_src->val = %x\n", op_src->val);
-		
+	Log("cpu.eip = %x\n", cpu.eip);
 }
 
 make_instr_helper(i)
