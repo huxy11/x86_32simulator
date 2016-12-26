feff9d8b20e56359b427430c5f30b4e1d3b0a570
tracer-ics2016 <tracer@njuics.org>
2016-12-26 02:23:19 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  02:23:19 up  1:47,  2 users,  load average: 0.29, 0.12, 0.07 17cb48f44623c76af57ec823d469ca91c8d89af5

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -6,6 +6,10 @@ static void do_execute() {
 	Log("op_src->val = %x\n", op_src->val);
 	Log("cpu.eip = %x\n", cpu.eip);
 	Log("esp = %x\n", REG(R_ESP));
+	REG(R_ESP) += 4;
+	swaddr_write(REG(R_ESP), 4, cpu.eip);
+	cpu.eip += op_src->val;
+	print_asm_template1();
 }
 
 make_instr_helper(i)
--- nemu/src/cpu/exec/stack/push-template.h
+++ nemu/src/cpu/exec/stack/push-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	//Log("type = %d\n\tsize = %d\n\tunion = %x\n\tval = %x\n\tstr = %s\n\t", op_src->type, op_src->size, op_src->imm, op_src->val, op_src->str);
 	REG(4) += DATA_BYTE;
 	swaddr_write(REG(4), DATA_BYTE, op_src->val);
-	print_asm_template2();
+	print_asm_template1();
 }
 
 make_instr_helper(r)
