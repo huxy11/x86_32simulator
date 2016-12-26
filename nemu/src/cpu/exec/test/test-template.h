75a1b3fffb6ebac13f660f06665d27cd888d42a6
tracer-ics2016 <tracer@njuics.org>
2016-12-26 07:38:40 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  07:38:40 up  7:02,  2 users,  load average: 0.14, 0.20, 0.22 e37dd1def9eae90b1fa59bb7b012a208175c4a56

--- nemu/src/cpu/exec/test/test-template.h
+++ nemu/src/cpu/exec/test/test-template.h
@@ -2,16 +2,37 @@
 
 #define instr test
 
+#if DATA_BYTE == 4
+#define MS 0x8000
+#elif DATA_BYTE == 2
+#define MS 0x80
+#endif
+
 static void do_execute() {
 	Log("src = %x\tstr = %s\n", op_src->val, op_src->str);
 	Log("src2 = %x\tstr = %s\n", op_src2->val, op_src2->str);
-	uint32_t re = op_src->val & op_src2->val;
+	uint32_t tmp = 1,re = op_src->val & op_src2->val;
+	uint8_t i;
 	Log("re = %x\n", re);
 	cpu._of = 0;
 	cpu._cf = 0;
+	cpu._sf = re & MS;
+	if (re == 0)
+		cpu._zf = 0;
+	else 
+		cpu._zf = 1;
+	cpu._pf = 0;
+	for (i = 0; i < DATA_BYTE * 8; i++) {
+		if (tmp & re) {
+			Log("%d is even\n", i);
+			cpu._pf = ~cpu._pf;
+		}
+		tmp <<= 1;
+	}
 	print_asm_template2src();
 }
 
 make_instr_helper(rm)
 
+#undef MS
 #include "cpu/exec/template-end.h"
