5b52de25161a6cb02cf88ebecd8606d929079257
tracer-ics2016 <tracer@njuics.org>
2016-12-27 07:47:01 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  07:47:01 up 1 day,  7:10,  2 users,  load average: 0.15, 0.16, 0.13 4987c8551105c57e5c0b077bd3b0d99f2918ef

--- nemu/src/cpu/exec/sub/sub-template.h
+++ nemu/src/cpu/exec/sub/sub-template.h
@@ -9,6 +9,11 @@ static void do_execute() {
 	Log("dest = %d\n", op_dest->imm);
 	uint32_t re = op_dest->val - op_src->simm;
 	Log("re = %d  0x%x\n", re, re);
+	check_pf(re);
+	check_sf(re);
+	check_zf(re);
+	Log("\nof = %d\nsf = %d\nzf = %d\naf =%d\npf = %d\ncf =%d\n",		\
+		 cpu._of, cpu._sf, cpu._zf, cpu._af, cpu._pf, cpu._cf);
 	print_asm_template2();
 }
 
