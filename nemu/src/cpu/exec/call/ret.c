816b891dbd5cf3de9add40427f1690483dd224f2
tracer-ics2016 <tracer@njuics.org>
2016-12-27 04:57:03 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  04:57:03 up 1 day,  4:20,  2 users,  load average: 0.32, 0.30, 0.26 e4e07de5f9535349e3c2bdb5613a059b76e76308

--- nemu/src/cpu/exec/call/ret.c
+++ nemu/src/cpu/exec/call/ret.c
@@ -4,7 +4,7 @@ make_helper(ret) {
 	swaddr_t re = swaddr_read(reg_l(R_ESP), 4);
 	cpu.eip = re;
 	reg_l(R_ESP) -= 4;
-	print_asm_template1();
+	print_asm("ret");
 	Log("cpu.eip = %x\n", cpu.eip);
 	return 0;
 }
