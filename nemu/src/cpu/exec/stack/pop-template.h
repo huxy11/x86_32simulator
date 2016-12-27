a10fd074eb043d10c61a771532196bc13f1d6af8
tracer-ics2016 <tracer@njuics.org>
2016-12-27 04:22:12 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  04:22:12 up 1 day,  3:45,  2 users,  load average: 0.43, 0.52, 0.37 6a8333e99d64b7e0357286274a1008bfb16634e2

--- nemu/src/cpu/exec/stack/pop-template.h
+++ nemu/src/cpu/exec/stack/pop-template.h
@@ -3,10 +3,10 @@
 #define instr pop
 
 static void do_execute() {
-	uint32_t re = swaddr_read(REG(R_ESP), DATA_BYTE);
+	uint32_t re = swaddr_read(REG(R_ESP), 4);
 	OPERAND_W(op_src, re);
 	Log("re = %x\n", re);
-	REG(R_ESP) -= DATA_BYTE;
+	REG(R_ESP) -= 4;
 	print_asm_template1();
 }
 
