3c37eaa3ac185264359aefa75e2667e1c8c4252c
tracer-ics2016 <tracer@njuics.org>
2016-12-27 03:49:19 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  03:49:19 up 1 day,  3:13,  2 users,  load average: 0.15, 0.18, 0.18 3d850aed6199f8c14b87b41a68a2c95dd0aa3ac3

--- nemu/src/cpu/exec/stack/pop-template.h
+++ nemu/src/cpu/exec/stack/pop-template.h
@@ -7,6 +7,7 @@ static void do_execute() {
 	Log("op_src = %s re = %x", op_src->str, re);
 	REG(R_ESP) -= DATA_BYTE;
 	op_src->val = 1;
+	Log("op_src = %x\n", op_src->val);
 	print_asm_template1();
 }
 
