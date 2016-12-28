b5161a19c40cbeb91382a84d39e9b8f4787550d9
tracer-ics2016 <tracer@njuics.org>
2016-12-28 03:59:05 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  03:59:05 up  1:14,  2 users,  load average: 0.09, 0.16, 0.21 46c4ab85b4d292a2db71c81ac7c61ddfee5a4dc

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -8,7 +8,7 @@ static void do_execute() {
 	op_src->val = ~op_src->val + 1;
 	Log("src->val = %x\tsrc->simm = %d\tsrc->str = %s\n", op_src->val, op_src->simm, op_src->str);
 	uint32_t re = op_dest->val + op_src->val;
-	Log("result = %x\n", re);
+	Log("result = %x  %d\n", re, re);
 	check_sf(re);
 	check_zf(re);
 	check_pf(re);
