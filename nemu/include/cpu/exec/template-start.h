a8143c1d63d8a2937fd15fe1c7376ab23fce1b9f
tracer-ics2016 <tracer@njuics.org>
2016-12-28 01:00:46 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  01:00:46 up 2 days, 24 min,  2 users,  load average: 0.23, 0.20, 0.22 97cc8e13f6b02bb92a9000e8251468279abb1060

--- nemu/include/cpu/exec/template-start.h
+++ nemu/include/cpu/exec/template-start.h
@@ -80,7 +80,7 @@
 										if (src_h != dest_h) {				\
 											DATA_TYPE re_h = 				\
 												re & MOST_SIGNIFICANCE;		\
-											Log("re_h %d  dest_h %d\n %d\t%d\n", re_h, dest_h, DATA_BYTE, MOST_SIGNIFICANCE);\
+											Log("re_h %d  dest_h %d\n %d\t%x\n", re_h, dest_h, DATA_BYTE, MOST_SIGNIFICANCE);\
 											if (re_h != dest_h)				\
 												cpu._of = 1;				\
 										}									\
