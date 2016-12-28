8689aa495cf060fed93f8897da93c9954a5b6a10
tracer-ics2016 <tracer@njuics.org>
2016-12-28 02:19:36 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  02:19:36 up 2 days,  1:43,  2 users,  load average: 0.16, 0.19, 0.22 b0eff487ea7aba516472b53b6653c2b346b533f1

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -3,11 +3,11 @@
 #define instr call
 
 static void do_execute() {
-	Log("cpu.eip = %x\n", cpu.eip);
+	//Log("cpu.eip = %x\n", cpu.eip);
 	REG(R_ESP) += 4;
 	swaddr_write(REG(R_ESP), 4, cpu.eip + DATA_BYTE + 1);
 	cpu.eip += op_src->val;
-	Log("cpu.eip = %x\n", cpu.eip);
+	//Log("cpu.eip = %x\n", cpu.eip);
 	print_asm_template1();
 }
 
