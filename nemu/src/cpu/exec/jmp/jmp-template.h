b667afce1f974cce553001899674d64c22f227b3
tracer-ics2016 <tracer@njuics.org>
2016-12-29 03:39:02 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  03:39:02 up 1 day, 53 min,  2 users,  load average: 1.07, 1.07, 1.05 1fa12ac3f0627f7d43ecd834929848454024965d

--- nemu/src/cpu/exec/jmp/jmp-template.h
+++ nemu/src/cpu/exec/jmp/jmp-template.h
@@ -4,6 +4,7 @@
 static void do_execute() {
 	cpu.eip += op_src->val;
 	Log("src = %s %d 0x%x\n", op_src->str, op_src->val, op_src->val);
+	OPERAND_W(op_dest, op_src->val);
 	print_asm_template1();
 }
 
