df388ee34891e3202f8e97fc02690d9b41e50085
tracer-ics2016 <tracer@njuics.org>
2016-12-29 08:46:43 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  08:46:43 up 1 day,  6:01,  2 users,  load average: 1.13, 1.23, 1.21 d0ce83858271b64210a19f847b3b403db344a12b

---
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -0,0 +1,17 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movzx
+
+static void do_execute() {
+	Log("op_src = %s %x\n", op_src->str, op_src->val);
+	Log("DATA_BYTE = %d\n", DATA_BYTE);
+	DATA_TYPE re = op_src->val;
+	OPERAND_W(op_dest, re);
+	print_asm_template2();
+}
+
+make_instr_helper(rm2r)
+
+
+#include "cpu/exec/template-end.h"
+
