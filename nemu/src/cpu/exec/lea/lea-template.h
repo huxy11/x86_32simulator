d88c60ab61d4cad8866a08f24cea9387524d8141
tracer-ics2016 <tracer@njuics.org>
2016-12-29 01:24:05 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  01:24:05 up 22:38,  2 users,  load average: 1.10, 1.12, 1.19 a4bc7ceca91eec13950181f9794f2369ffe2fd82

--- nemu/src/cpu/exec/lea/lea-template.h
+++ nemu/src/cpu/exec/lea/lea-template.h
@@ -3,9 +3,9 @@
 #define instr lea
 
 static void do_execute() {
-
 	Log("src = %s %x\n", op_src->str, op_src->addr);
 	Log("dest = %s %x\n", op_dest->str, op_dest->val);
+	OPERAND_W(op_dest,op_src->addr);
 	print_asm_template2();
 }
 
