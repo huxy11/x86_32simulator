1f2b7de8bca4ce18b6d9a5f8cb1e0c207a70659c
tracer-ics2016 <tracer@njuics.org>
2016-12-29 09:12:21 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  09:12:21 up 1 day,  6:27,  2 users,  load average: 1.03, 1.05, 1.11 39983d5796161c314518d20f8181973842e015e

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -17,8 +17,11 @@ static void do_execute() {
 	check_sub_af(op_src->val, op_dest->val);
 	print_asm_template2();
 }
+#if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(si2rm)
+#endif
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
+make_instr_helper(i2a)
 
 #include "cpu/exec/template-end.h"
