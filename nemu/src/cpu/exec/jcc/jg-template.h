4367c49811444313cde01acc7022bc31cb70bffc
tracer-ics2016 <tracer@njuics.org>
2016-12-30 01:10:15 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  01:10:15 up 1 day, 22:24,  2 users,  load average: 1.00, 1.04, 1.12 790005b16d6bb6fa6af6af463c1dcd512d050d32

--- nemu/src/cpu/exec/jcc/jg-template.h
+++ nemu/src/cpu/exec/jcc/jg-template.h
@@ -3,7 +3,7 @@
 #define instr jg
 
 static void do_execute() {
-	if((cpu._zf !=0)&&(cpu._of != cpu._sf))
+	if((cpu._zf ==0)&&(cpu._of == cpu._sf))
 		cpu.eip += op_src->val;
 
 	print_asm_template1();
