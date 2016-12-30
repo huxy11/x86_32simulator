1ea3cfd0a42bd54091bf1994d5ec35bc03ca01d3
tracer-ics2016 <tracer@njuics.org>
2016-12-30 01:39:24 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  01:39:24 up 1 day, 22:54,  2 users,  load average: 1.01, 1.06, 1.08 e0744940a6de906c86db6c37600fe155f0cec88c

--- nemu/src/cpu/exec/sub/sub-template.h
+++ nemu/src/cpu/exec/sub/sub-template.h
@@ -3,10 +3,10 @@
 #define instr sub
 
 static void do_execute() {
-	Log("src = %s   0x%x\n", op_src->str, op_src->val);
-	Log("src = %d \n", op_src->simm);
-	Log("dest = %s   0x%x\n", op_dest->str, op_dest->val);
-	Log("dest = %d\n", op_dest->imm);
+//	Log("src = %s   0x%x\n", op_src->str, op_src->val);
+//	Log("src = %d \n", op_src->simm);
+//	Log("dest = %s   0x%x\n", op_dest->str, op_dest->val);
+//	Log("dest = %d\n", op_dest->imm);
 	uint32_t re = op_dest->val - op_src->val;
 	Log("re = %d 0x%x\n", re, re);
 	check_pf(re);
@@ -16,8 +16,7 @@ static void do_execute() {
 	check_sub_of(op_src->val, op_dest->val, re);
 	check_sub_af(op_src->val, op_dest->val);
 	
-	Log("\nof = %d\nsf = %d\nzf = %d\naf =%d\npf = %d\ncf =%d\n",		\
-		 cpu._of, cpu._sf, cpu._zf, cpu._af, cpu._pf, cpu._cf);
+//	Log("\nof = %d\nsf = %d\nzf = %d\naf =%d\npf = %d\ncf =%d\n",cpu._of, cpu._sf, cpu._zf, cpu._af, cpu._pf, cpu._cf);
 
 	OPERAND_W(op_dest, re);
 	print_asm_template2();
