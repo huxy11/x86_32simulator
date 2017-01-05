a9ce070c4a3ab71a3e5c9a175d3ab5dd36d386a9
tracer-ics2016 <tracer@njuics.org>
2017-01-05 08:32:40 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  08:32:40 up  8:08,  3 users,  load average: 0.01, 0.07, 0.12 c1421725f5b06cbceebc894b455632785d8dbc25

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -4,6 +4,8 @@
 
 static void do_execute() {
 	DATA_TYPE_S re = op_src->simm;
+	Log("src = %s 0x%x %d\n", op_src->str, op_src->val, op_src->simm);
+	Log("dest = %s 0x %x %d\n", op_dest->str, op_dest->val, op_dest->simm);
 	OPERAND_W(op_dest, re);
 	print_asm_template2();
 }
