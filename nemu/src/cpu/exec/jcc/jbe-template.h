bf9c591116971ed4d704a95af1dd1a401bfff955
tracer-ics2016 <tracer@njuics.org>
2017-01-05 03:02:23 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  03:02:23 up  2:38,  2 users,  load average: 0.06, 0.18, 0.17 74d3d5e7565c3f9bba0b2bf8b53aa90d905dd2b7

--- nemu/src/cpu/exec/jcc/jbe-template.h
+++ nemu/src/cpu/exec/jcc/jbe-template.h
@@ -3,7 +3,8 @@
 #define instr jbe
 
 static void do_execute() {
-//	Log("op_src->val = %x\top_src->str = %s\n", op_src->val, op_src->str);
+	Log("op_src->val = %x\top_src->str = %s\n", op_src->val, op_src->str);
+	Log("op_src->simm = %d\n", op_src->simm);
 	if ((cpu._zf == 1) || (cpu._cf == 1))
 		cpu.eip += op_src->simm;
 	print_asm_template1();
