da5ed04d804aef5bb0cc9f7449c87566fdcb3449
tracer-ics2016 <tracer@njuics.org>
2017-01-05 03:31:09 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  03:31:09 up  3:07,  2 users,  load average: 0.08, 0.10, 0.13 766bae1beafedc0e46d0341221dd3b69f91908d0

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -1,17 +1,14 @@
 #include "cpu/exec/template-start.h"
 
-#define instr movzx
+#define instr movsx
 
 static void do_execute() {
-//	Log("op_src = %s 0x%x\n", op_src->str, op_src->val);
-//	Log("DATA_BYTE = %d\n", DATA_BYTE);
-	DATA_TYPE re = op_src->val;
+	Log("op_src = 0x%x %d\n", op_src->val, op_src->simm);
+	DATA_TYPE_S re = op_src->simm;
 	OPERAND_W(op_dest, re);
 	print_asm_template2();
 }
 
 make_instr_helper(rm2r)
 
-
 #include "cpu/exec/template-end.h"
-
