1567b6ca1198942b2a7dbd325b0ade5d9ca03ebe
tracer-ics2016 <tracer@njuics.org>
2017-01-06 03:37:27 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  03:37:27 up 1 day,  3:13,  3 users,  load average: 0.13, 0.17, 0.14 ca5d8ee40f3681322ffd04e962ee628341836

--- nemu/src/cpu/exec/data-mov/movs-template.h
+++ nemu/src/cpu/exec/data-mov/movs-template.h
@@ -3,11 +3,11 @@
 #define instr movs
 
 static void do_execute() {
-	DATA_TYPE src = swaddr_read(REG(R_ESI), DATA_BYTE);
-	swaddr_write(REG(R_EDI), DATA_BYTE, src);
+	DATA_TYPE src = swaddr_read(reg_l(R_ESI), DATA_BYTE);
+	swaddr_write(reg_l(R_EDI), DATA_BYTE, src);
 	Log("DATA_BYTE = %d\tsrc = 0x%x\n", DATA_BYTE, src);
-	REG(R_EDI) += DATA_BYTE;
-	REG(R_ESI) += DATA_BYTE;
+	reg_l(R_EDI) += DATA_BYTE;
+	reg_l(R_ESI) += DATA_BYTE;
 }
 
 make_helper(concat(movs_, SUFFIX)) {
