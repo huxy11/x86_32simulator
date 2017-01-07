cae1c920c27dd5b0a12ce9abde798695f4d8cb20
tracer-ics2016 <tracer@njuics.org>
2017-01-07 02:39:21 +0000

    >  run 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  02:39:21 up 2 days,  2:15,  2 users,  load average: 0.21, 0.30, 0.25 123656d3f25f9906ca4a1c3cba1dc62fb10933c

--- testcase/src/mov-c.c
+++ testcase/src/mov-c.c
@@ -5,6 +5,12 @@ int A[10];
 int b;
 
 int main() {
+
+
+	
+	FLOAT c = f2F(6.2);
+	A[0] = c;
+	/*
 	A[0] = 0;
 	A[1] = 1;
 	A[2] = 2;
@@ -14,8 +20,6 @@ int main() {
 	b = A[3];
 	A[5] = b;
 
-	FLOAT c = f2F(6.2);
-	A[6] = c;
 
 	nemu_assert(A[0] == 0);
 	nemu_assert(A[1] == 1);
@@ -24,6 +28,7 @@ int main() {
 	nemu_assert(A[4] == 4);
 	nemu_assert(b == 3);
 	nemu_assert(A[5] == 3);
+	*/
 
 	return 0;
 }
