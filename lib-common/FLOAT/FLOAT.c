b73e6b780a2b2be02a5a5a40f43674afaca8f606
tracer-ics2016 <tracer@njuics.org>
2017-01-09 08:38:22 +0000

    >  compile NEMU 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  08:38:22 up 4 days,  8:14,  2 users,  load average: 0.07, 0.23, 0.34 d2bf8876e5c94872c7a2fbc51a4f3fadd9ab431c

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -4,6 +4,7 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 	long long rel = (long long)a * (long long)b;
 	int *re1 = (int*) &rel;
 	int *re2 = re1 + 1;
+	nemu_assert(re2 != 0);
 	int res = ((*re1 & 0xffff0000) >> 16) | ((*re2 & 0xffff) >> 16);
 	return res;
 }
