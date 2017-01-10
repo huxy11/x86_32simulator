72982cd34b64c5e4b43f4b9c5a28f990ed035860
tracer-ics2016 <tracer@njuics.org>
2017-01-10 02:39:17 +0000

    >  run 233333333 huxy11 Linux 2b664b926703 3.13.0-101-generic #148-Ubuntu SMP Thu Oct 20 22:08:32 UTC 2016 x86_64 GNU/Linux  02:39:17 up 5 days,  2:15,  2 users,  load average: 0.24, 0.40, 0.38 9730063e68382c986a927f453a4fd20597fc248c

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -10,29 +10,12 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	/* Dividing two 64-bit integers needs the support of another library
-	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-	 * here. A "64/32" division is enough.
-	 *
-	 * To perform a "64/32" division, you can use the x86 instruction
-	 * `div' or `idiv' by inline assembly. We provide a template for you
-	 * to prevent you from uncessary details.
-	 *
-	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-	 *
-	 * If you want to use the template above, you should fill the "???"
-	 * correctly. For more information, please read the i386 manual for
-	 * division instructions, and search the Internet about "inline assembly".
-	 * It is OK not to use the template above, but you should figure
-	 * out another way to perform the division.
-	 */
-//	long long rel = ((long long)a << 16) / (long long)b;
-//	long long rel = 0;
-
 	long long rel = 0, al = (long long)a << 16;
 	int *a1 = (int*)&al;
 	int *a2 = a1 + 1;
+
+	nemu_assert( *a1 == 0x33330000);
+	nemu_assert( *a2 == 0x1);
 
 	int *re1 = (int*)&rel;
 	int *re2 = re1 + 1;
